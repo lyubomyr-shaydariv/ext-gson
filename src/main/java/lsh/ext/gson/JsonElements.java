76ff97d8656ed5fa8024a3cd1b8b277cef75447c
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2017-03-08 10:49:18 +0200

    Added JsonElement factory methods

--- src/main/java/lsh/ext/gson/JsonElements.java
+++ src/main/java/lsh/ext/gson/JsonElements.java
@@ -1,9 +1,14 @@
 package lsh.ext.gson;
 
 import java.util.Map.Entry;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import com.google.gson.TypeAdapter;
 
 import static lsh.ext.gson.JsonObjectMergePredicates.alwaysReplaceLeft;
@@ -17,6 +22,547 @@ import static lsh.ext.gson.JsonObjectMergePredicates.alwaysReplaceLeft;
 public final class JsonElements {
 
 	private JsonElements() {
+	}
+
+	/**
+	 * Represents a JSON {@code null}.
+	 *
+	 * @return {@link JsonNull#INSTANCE} singleton.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonNull jsonNull() {
+		return JsonNull.INSTANCE;
+	}
+
+	/**
+	 * Represents a JSON primitive value {@code true} or {@code false}.
+	 *
+	 * @param b A boolean value.
+	 *
+	 * @return A new JSON primitive.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonPrimitive jsonPrimitive(@Nonnull final Boolean b) {
+		return new JsonPrimitive(b);
+	}
+
+	/**
+	 * Represents a JSON primitive numeric value.
+	 *
+	 * @param n A numeric value.
+	 *
+	 * @return A new JSON primitive.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonPrimitive jsonPrimitive(@Nonnull final Number n) {
+		return new JsonPrimitive(n);
+	}
+
+	/**
+	 * Represents a JSON primitive string value.
+	 *
+	 * @param s A string value.
+	 *
+	 * @return A new JSON primitive.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonPrimitive jsonPrimitive(@Nonnull final String s) {
+		return new JsonPrimitive(s);
+	}
+
+	/**
+	 * Represents a JSON primitive single character value.
+	 *
+	 * @param c A character value.
+	 *
+	 * @return A new JSON primitive.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonPrimitive jsonPrimitive(@Nonnull final Character c) {
+		return new JsonPrimitive(c);
+	}
+
+	/**
+	 * Creates a new JSON object: {@code {}}.
+	 *
+	 * @return An empty JSON object.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonObject jsonObject() {
+		return new JsonObject();
+	}
+
+	/**
+	 * Creates a new JSON object: {@code {k1: v1}}.
+	 *
+	 * @param k1 Key 1 name.
+	 * @param v1 Value 1.
+	 *
+	 * @return A JSON object with one key-value pair.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonObject jsonObject(
+			@Nonnull final String k1, @Nullable final JsonElement v1
+	) {
+		final JsonObject jsonObject = new JsonObject();
+		jsonObject.add(k1, v1);
+		return jsonObject;
+	}
+
+	/**
+	 * Creates a new JSON object: {@code {k1: v1, k2: v2}}.
+	 *
+	 * @param k1 Key 1 name.
+	 * @param v1 Value 1.
+	 * @param k2 Key 2 name.
+	 * @param v2 Value 2.
+	 *
+	 * @return A JSON object with two key-value pairs.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonObject jsonObject(
+			@Nonnull final String k1, @Nullable final JsonElement v1,
+			@Nonnull final String k2, @Nullable final JsonElement v2
+	) {
+		final JsonObject jsonObject = new JsonObject();
+		jsonObject.add(k1, v1);
+		jsonObject.add(k2, v2);
+		return jsonObject;
+	}
+
+	/**
+	 * Creates a new JSON object: {@code {k1: v1, k2: v2, k3: v3}}.
+	 *
+	 * @param k1 Key 1 name.
+	 * @param v1 Value 1.
+	 * @param k2 Key 2 name.
+	 * @param v2 Value 2.
+	 * @param k3 Key 3 name.
+	 * @param v3 Value 3.
+	 *
+	 * @return A JSON object with three key-value pairs.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonObject jsonObject(
+			@Nonnull final String k1, @Nullable final JsonElement v1,
+			@Nonnull final String k2, @Nullable final JsonElement v2,
+			@Nonnull final String k3, @Nullable final JsonElement v3
+	) {
+		final JsonObject jsonObject = new JsonObject();
+		jsonObject.add(k1, v1);
+		jsonObject.add(k2, v2);
+		jsonObject.add(k3, v3);
+		return jsonObject;
+	}
+
+	/**
+	 * Creates a new JSON object: {@code {k1: v1, k2: v2, k3: v3, k4: v4}}.
+	 *
+	 * @param k1 Key 1 name.
+	 * @param v1 Value 1.
+	 * @param k2 Key 2 name.
+	 * @param v2 Value 2.
+	 * @param k3 Key 3 name.
+	 * @param v3 Value 3.
+	 * @param k4 Key 4 name.
+	 * @param v4 Value 4.
+	 *
+	 * @return A JSON object with four key-value pairs.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonObject jsonObject(
+			@Nonnull final String k1, @Nullable final JsonElement v1,
+			@Nonnull final String k2, @Nullable final JsonElement v2,
+			@Nonnull final String k3, @Nullable final JsonElement v3,
+			@Nonnull final String k4, @Nullable final JsonElement v4
+	) {
+		final JsonObject jsonObject = new JsonObject();
+		jsonObject.add(k1, v1);
+		jsonObject.add(k2, v2);
+		jsonObject.add(k3, v3);
+		jsonObject.add(k4, v4);
+		return jsonObject;
+	}
+
+	/**
+	 * Creates a new JSON object: {@code {k1: v1, k2: v2, k3: v3, k4: v4, k5: v5}}.
+	 *
+	 * @param k1 Key 1 name.
+	 * @param v1 Value 1.
+	 * @param k2 Key 2 name.
+	 * @param v2 Value 2.
+	 * @param k3 Key 3 name.
+	 * @param v3 Value 3.
+	 * @param k4 Key 4 name.
+	 * @param v4 Value 4.
+	 * @param k5 Key 5 name.
+	 * @param v5 Value 5.
+	 *
+	 * @return A JSON object with five key-value pairs.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonObject jsonObject(
+			@Nonnull final String k1, @Nullable final JsonElement v1,
+			@Nonnull final String k2, @Nullable final JsonElement v2,
+			@Nonnull final String k3, @Nullable final JsonElement v3,
+			@Nonnull final String k4, @Nullable final JsonElement v4,
+			@Nonnull final String k5, @Nullable final JsonElement v5
+	) {
+		final JsonObject jsonObject = new JsonObject();
+		jsonObject.add(k1, v1);
+		jsonObject.add(k2, v2);
+		jsonObject.add(k3, v3);
+		jsonObject.add(k4, v4);
+		jsonObject.add(k5, v5);
+		return jsonObject;
+	}
+
+	/**
+	 * Creates a new JSON array: {@code []}.
+	 *
+	 * @return An empty JSON array.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonArray jsonArray() {
+		return new JsonArray();
+	}
+
+	/**
+	 * Creates a new JSON array: {@code [e1]}.
+	 *
+	 * @param e1 Element 1.
+	 *
+	 * @return A JSON array with one element.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonArray jsonArray(
+			@Nullable final JsonElement e1
+	) {
+		final JsonArray jsonArray = new JsonArray();
+		jsonArray.add(e1);
+		return jsonArray;
+	}
+
+	/**
+	 * Creates a new JSON array: {@code [e1, e2]}.
+	 *
+	 * @param e1 Element 1.
+	 * @param e2 Element 2.
+	 *
+	 * @return A JSON array with two elements.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonArray jsonArray(
+			@Nullable final JsonElement e1,
+			@Nullable final JsonElement e2
+	) {
+		final JsonArray jsonArray = new JsonArray();
+		jsonArray.add(e1);
+		jsonArray.add(e2);
+		return jsonArray;
+	}
+
+	/**
+	 * Creates a new JSON array: {@code [e1, e2, e3]}.
+	 *
+	 * @param e1 Element 1.
+	 * @param e2 Element 2.
+	 * @param e3 Element 3.
+	 *
+	 * @return A JSON array with three elements.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonArray jsonArray(
+			@Nullable final JsonElement e1,
+			@Nullable final JsonElement e2,
+			@Nullable final JsonElement e3
+	) {
+		final JsonArray jsonArray = new JsonArray();
+		jsonArray.add(e1);
+		jsonArray.add(e2);
+		jsonArray.add(e3);
+		return jsonArray;
+	}
+
+	/**
+	 * Creates a new JSON array: {@code [e1, e2, e3, e4]}.
+	 *
+	 * @param e1 Element 1.
+	 * @param e2 Element 2.
+	 * @param e3 Element 3.
+	 * @param e4 Element 4.
+	 *
+	 * @return A JSON array with four elements.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonArray jsonArray(
+			@Nullable final JsonElement e1,
+			@Nullable final JsonElement e2,
+			@Nullable final JsonElement e3,
+			@Nullable final JsonElement e4
+	) {
+		final JsonArray jsonArray = new JsonArray();
+		jsonArray.add(e1);
+		jsonArray.add(e2);
+		jsonArray.add(e3);
+		jsonArray.add(e4);
+		return jsonArray;
+	}
+
+	/**
+	 * Creates a new JSON array: {@code [e1, e2, e3, e4, e5]}.
+	 *
+	 * @param e1 Element 1.
+	 * @param e2 Element 2.
+	 * @param e3 Element 3.
+	 * @param e4 Element 4.
+	 * @param e5 Element 5.
+	 *
+	 * @return A JSON array with five elements.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonArray jsonArray(
+			@Nullable final JsonElement e1,
+			@Nullable final JsonElement e2,
+			@Nullable final JsonElement e3,
+			@Nullable final JsonElement e4,
+			@Nullable final JsonElement e5
+	) {
+		final JsonArray jsonArray = new JsonArray();
+		jsonArray.add(e1);
+		jsonArray.add(e2);
+		jsonArray.add(e3);
+		jsonArray.add(e4);
+		jsonArray.add(e5);
+		return jsonArray;
+	}
+
+	/**
+	 * Creates a new JSON array: {@code [e1, e2, e3, e4, e5, e6]}.
+	 *
+	 * @param e1 Element 1.
+	 * @param e2 Element 2.
+	 * @param e3 Element 3.
+	 * @param e4 Element 4.
+	 * @param e5 Element 5.
+	 * @param e6 Element 6.
+	 *
+	 * @return A JSON array with six elements.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonArray jsonArray(
+			@Nullable final JsonElement e1,
+			@Nullable final JsonElement e2,
+			@Nullable final JsonElement e3,
+			@Nullable final JsonElement e4,
+			@Nullable final JsonElement e5,
+			@Nullable final JsonElement e6
+	) {
+		final JsonArray jsonArray = new JsonArray();
+		jsonArray.add(e1);
+		jsonArray.add(e2);
+		jsonArray.add(e3);
+		jsonArray.add(e4);
+		jsonArray.add(e5);
+		jsonArray.add(e6);
+		return jsonArray;
+	}
+
+	/**
+	 * Creates a new JSON array: {@code [e1, e2, e3, e4, e5, e6, e7]}.
+	 *
+	 * @param e1 Element 1.
+	 * @param e2 Element 2.
+	 * @param e3 Element 3.
+	 * @param e4 Element 4.
+	 * @param e5 Element 5.
+	 * @param e6 Element 6.
+	 * @param e7 Element 7.
+	 *
+	 * @return A JSON array with seven elements.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonArray jsonArray(
+			@Nullable final JsonElement e1,
+			@Nullable final JsonElement e2,
+			@Nullable final JsonElement e3,
+			@Nullable final JsonElement e4,
+			@Nullable final JsonElement e5,
+			@Nullable final JsonElement e6,
+			@Nullable final JsonElement e7
+	) {
+		final JsonArray jsonArray = new JsonArray();
+		jsonArray.add(e1);
+		jsonArray.add(e2);
+		jsonArray.add(e3);
+		jsonArray.add(e4);
+		jsonArray.add(e5);
+		jsonArray.add(e6);
+		jsonArray.add(e7);
+		return jsonArray;
+	}
+
+	/**
+	 * Creates a new JSON array: {@code [e1, e2, e3, e4, e5, e6, e7, e8]}.
+	 *
+	 * @param e1 Element 1.
+	 * @param e2 Element 2.
+	 * @param e3 Element 3.
+	 * @param e4 Element 4.
+	 * @param e5 Element 5.
+	 * @param e6 Element 6.
+	 * @param e7 Element 7.
+	 * @param e8 Element 8.
+	 *
+	 * @return A JSON array with eight elements.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	public static JsonArray jsonArray(
+			final JsonElement e1,
+			final JsonElement e2,
+			final JsonElement e3,
+			final JsonElement e4,
+			final JsonElement e5,
+			final JsonElement e6,
+			final JsonElement e7,
+			final JsonElement e8
+	) {
+		final JsonArray jsonArray = new JsonArray();
+		jsonArray.add(e1);
+		jsonArray.add(e2);
+		jsonArray.add(e3);
+		jsonArray.add(e4);
+		jsonArray.add(e5);
+		jsonArray.add(e6);
+		jsonArray.add(e7);
+		jsonArray.add(e8);
+		return jsonArray;
+	}
+
+	/**
+	 * Creates a new JSON array: {@code [e1, e2, e3, e4, e5, e6, e7, e8, e9]}.
+	 *
+	 * @param e1 Element 1.
+	 * @param e2 Element 2.
+	 * @param e3 Element 3.
+	 * @param e4 Element 4.
+	 * @param e5 Element 5.
+	 * @param e6 Element 6.
+	 * @param e7 Element 7.
+	 * @param e8 Element 8.
+	 * @param e9 Element 9.
+	 *
+	 * @return A JSON array with nine elements.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonArray jsonArray(
+			@Nullable final JsonElement e1,
+			@Nullable final JsonElement e2,
+			@Nullable final JsonElement e3,
+			@Nullable final JsonElement e4,
+			@Nullable final JsonElement e5,
+			@Nullable final JsonElement e6,
+			@Nullable final JsonElement e7,
+			@Nullable final JsonElement e8,
+			@Nullable final JsonElement e9
+	) {
+		final JsonArray jsonArray = new JsonArray();
+		jsonArray.add(e1);
+		jsonArray.add(e2);
+		jsonArray.add(e3);
+		jsonArray.add(e4);
+		jsonArray.add(e5);
+		jsonArray.add(e6);
+		jsonArray.add(e7);
+		jsonArray.add(e8);
+		jsonArray.add(e9);
+		return jsonArray;
+	}
+
+	/**
+	 * Creates a new JSON array: {@code [e1, e2, e3, e4, e5, e6, e7, e8, e9, e10]}.
+	 *
+	 * @param e1  Element 1.
+	 * @param e2  Element 2.
+	 * @param e3  Element 3.
+	 * @param e4  Element 4.
+	 * @param e5  Element 5.
+	 * @param e6  Element 6.
+	 * @param e7  Element 7.
+	 * @param e8  Element 8.
+	 * @param e9  Element 9.
+	 * @param e10 Element 10.
+	 *
+	 * @return A JSON array with ten elements.
+	 *
+	 * @since 0-SNAPSHOT
+	 */
+	@Nonnull
+	public static JsonArray jsonArray(
+			@Nullable final JsonElement e1,
+			@Nullable final JsonElement e2,
+			@Nullable final JsonElement e3,
+			@Nullable final JsonElement e4,
+			@Nullable final JsonElement e5,
+			@Nullable final JsonElement e6,
+			@Nullable final JsonElement e7,
+			@Nullable final JsonElement e8,
+			@Nullable final JsonElement e9,
+			@Nullable final JsonElement e10
+	) {
+		final JsonArray jsonArray = new JsonArray();
+		jsonArray.add(e1);
+		jsonArray.add(e2);
+		jsonArray.add(e3);
+		jsonArray.add(e4);
+		jsonArray.add(e5);
+		jsonArray.add(e6);
+		jsonArray.add(e7);
+		jsonArray.add(e8);
+		jsonArray.add(e9);
+		jsonArray.add(e10);
+		return jsonArray;
 	}
 
 	/**
--- src/test/java/lsh/ext/gson/JsonElementsTest.java
+++ src/test/java/lsh/ext/gson/JsonElementsTest.java
@@ -1,20 +1,29 @@
 package lsh.ext.gson;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import org.hamcrest.Matcher;
 import org.junit.Test;
 
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
+import static lsh.ext.gson.JsonElements.jsonArray;
+import static lsh.ext.gson.JsonElements.jsonNull;
+import static lsh.ext.gson.JsonElements.jsonObject;
+import static lsh.ext.gson.JsonElements.jsonPrimitive;
 import static lsh.ext.gson.JsonElements.mergeIntoLeft;
 import static lsh.ext.gson.JsonElements.mergeIntoNew;
-import static lsh.ext.gson.JsonObjectBuilders.jsonObject;
 import static lsh.ext.gson.JsonObjectMergePredicates.alwaysReplaceLeft;
 import static lsh.ext.gson.JsonObjectMergePredicates.neverReplaceLeft;
 import static lsh.ext.gson.MoreMatchers.hasProperty;
 import static lsh.ext.gson.MoreMatchers.refersFirst;
 import static lsh.ext.gson.MoreMatchers.refersNone;
 import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -25,9 +34,297 @@ public final class JsonElementsTest {
 	private static final String K1 = "foo";
 	private static final String K2 = "bar";
 	private static final String K3 = "baz";
+	private static final String K4 = "qux";
+	private static final String K5 = "quux";
+	private static final String K6 = "corge";
+	private static final String K7 = "grault";
+	private static final String K8 = "garply";
+	private static final String K9 = "waldo";
+	private static final String K10 = "fred";
 
 	private static final JsonPrimitive l = new JsonPrimitive("L");
 	private static final JsonPrimitive r = new JsonPrimitive("R");
+
+	@Test
+	public void testJsonNull() {
+		assertThat(jsonNull(), is(JsonNull.INSTANCE));
+	}
+
+	@Test
+	public void testJsonPrimitiveForBooleans() {
+		assertThat(jsonPrimitive(TRUE), is(new JsonPrimitive(true)));
+		assertThat(jsonPrimitive(FALSE), is(new JsonPrimitive(false)));
+	}
+
+	@Test(expected = NullPointerException.class)
+	@SuppressWarnings("ConstantConditions")
+	public void testJsonPrimitiveForBooleansAsNull() {
+		jsonPrimitive((Boolean) null);
+	}
+
+	@Test
+	public void testJsonPrimitiveForNumbers() {
+		assertThat(jsonPrimitive((byte) 1), is(new JsonPrimitive(1)));
+		assertThat(jsonPrimitive((short) 2), is(new JsonPrimitive(2)));
+		assertThat(jsonPrimitive(3), is(new JsonPrimitive(3)));
+		assertThat(jsonPrimitive(4L), is(new JsonPrimitive(4)));
+		assertThat(jsonPrimitive(5F), is(new JsonPrimitive(5)));
+		assertThat(jsonPrimitive(6D), is(new JsonPrimitive(6)));
+	}
+
+	@Test(expected = NullPointerException.class)
+	@SuppressWarnings("ConstantConditions")
+	public void testJsonPrimitiveForNumbersAsNull() {
+		jsonPrimitive((Number) null);
+	}
+
+	@Test
+	public void testJsonPrimitivesForStrings() {
+		assertThat(jsonPrimitive(K1), is(new JsonPrimitive(K1)));
+		assertThat(jsonPrimitive(K2), is(new JsonPrimitive(K2)));
+	}
+
+	@Test(expected = NullPointerException.class)
+	@SuppressWarnings("ConstantConditions")
+	public void testJsonPrimitivesForStringsAsNull() {
+		jsonPrimitive((String) null);
+	}
+
+	@Test
+	public void testJsonPrimitivesForCharacters() {
+		assertThat(jsonPrimitive('f'), is(new JsonPrimitive('f')));
+		assertThat(jsonPrimitive('b'), is(new JsonPrimitive('b')));
+	}
+
+	@Test(expected = NullPointerException.class)
+	@SuppressWarnings("ConstantConditions")
+	public void testJsonPrimitivesForCharactersAsNull() {
+		jsonPrimitive((Character) null);
+	}
+
+	@Test
+	public void testJsonObject() {
+		assertThat(jsonObject(), is(new JsonObject()));
+	}
+
+	@Test
+	public void testJsonObject1() {
+		final JsonObject o1 = new JsonObject();
+		o1.addProperty(K1, 1);
+		assertThat(jsonObject(K1, jsonPrimitive(1)), is(o1));
+		final JsonObject o2 = new JsonObject();
+		o2.addProperty(K1, (String) null);
+		assertThat(jsonObject(K1, null), is(o2));
+	}
+
+	@Test(expected = NullPointerException.class)
+	@SuppressWarnings("ConstantConditions")
+	public void testJsonObject1AsNull() {
+		jsonObject(null, jsonPrimitive(K1));
+	}
+
+	@Test
+	public void testJsonObject2() {
+		final JsonObject o1 = new JsonObject();
+		o1.addProperty(K1, 1);
+		o1.addProperty(K2, 2);
+		assertThat(jsonObject(K1, jsonPrimitive(1), K2, jsonPrimitive(2)), is(o1));
+		final JsonObject o2 = new JsonObject();
+		o2.addProperty(K1, (String) null);
+		o2.addProperty(K2, (String) null);
+		assertThat(jsonObject(K1, null, K2, null), is(o2));
+	}
+
+	@Test(expected = NullPointerException.class)
+	@SuppressWarnings("ConstantConditions")
+	public void testJsonObject2AsNull() {
+		jsonObject(null, jsonPrimitive(K1), null, jsonPrimitive(K2));
+	}
+
+	@Test
+	public void testJsonObject3() {
+		final JsonObject o1 = new JsonObject();
+		o1.addProperty(K1, 1);
+		o1.addProperty(K2, 2);
+		o1.addProperty(K3, 3);
+		assertThat(jsonObject(K1, jsonPrimitive(1), K2, jsonPrimitive(2), K3, jsonPrimitive(3)), is(o1));
+		final JsonObject o2 = new JsonObject();
+		o2.addProperty(K1, (String) null);
+		o2.addProperty(K2, (String) null);
+		o2.addProperty(K3, (String) null);
+		assertThat(jsonObject(K1, null, K2, null, K3, null), is(o2));
+	}
+
+	@Test(expected = NullPointerException.class)
+	@SuppressWarnings("ConstantConditions")
+	public void testJsonObject3AsNull() {
+		jsonObject(null, jsonPrimitive(K1), null, jsonPrimitive(K2), null, jsonPrimitive(K3));
+	}
+
+	@Test
+	public void testJsonObject4() {
+		final JsonObject o1 = new JsonObject();
+		o1.addProperty(K1, 1);
+		o1.addProperty(K2, 2);
+		o1.addProperty(K3, 3);
+		o1.addProperty(K4, 4);
+		assertThat(jsonObject(K1, jsonPrimitive(1), K2, jsonPrimitive(2), K3, jsonPrimitive(3), K4, jsonPrimitive(4)), is(o1));
+		final JsonObject o2 = new JsonObject();
+		o2.addProperty(K1, (String) null);
+		o2.addProperty(K2, (String) null);
+		o2.addProperty(K3, (String) null);
+		o2.addProperty(K4, (String) null);
+		assertThat(jsonObject(K1, null, K2, null, K3, null, K4, null), is(o2));
+	}
+
+	@Test(expected = NullPointerException.class)
+	@SuppressWarnings("ConstantConditions")
+	public void testJsonObject4AsNull() {
+		jsonObject(null, jsonPrimitive(K1), null, jsonPrimitive(K2), null, jsonPrimitive(K3), null, jsonPrimitive(K4));
+	}
+
+	@Test
+	public void testJsonObject5() {
+		final JsonObject o1 = new JsonObject();
+		o1.addProperty(K1, 1);
+		o1.addProperty(K2, 2);
+		o1.addProperty(K3, 3);
+		o1.addProperty(K4, 4);
+		o1.addProperty(K5, 5);
+		assertThat(jsonObject(K1, jsonPrimitive(1), K2, jsonPrimitive(2), K3, jsonPrimitive(3), K4, jsonPrimitive(4), K5, jsonPrimitive(5)), is(o1));
+		final JsonObject o2 = new JsonObject();
+		o2.addProperty(K1, (String) null);
+		o2.addProperty(K2, (String) null);
+		o2.addProperty(K3, (String) null);
+		o2.addProperty(K4, (String) null);
+		o2.addProperty(K5, (String) null);
+		assertThat(jsonObject(K1, null, K2, null, K3, null, K4, null, K5, null), is(o2));
+	}
+
+	@Test(expected = NullPointerException.class)
+	@SuppressWarnings("ConstantConditions")
+	public void testJsonObject5AsNull() {
+		jsonObject(null, jsonPrimitive(K1), null, jsonPrimitive(K2), null, jsonPrimitive(K3), null, jsonPrimitive(K4), null, jsonPrimitive(K5));
+	}
+
+	@Test
+	public void testJsonArray() {
+		assertThat(
+				jsonArray(),
+				is(stringJsonArray())
+		);
+	}
+
+	@Test
+	public void testJsonArray1() {
+		assertThat(
+				jsonArray(jsonPrimitive(K1)),
+				is(stringJsonArray(K1))
+		);
+		assertThat(
+				jsonArray(null),
+				is(stringJsonArray((String) null))
+		);
+	}
+
+	@Test
+	public void testJsonArray2() {
+		assertThat(
+				jsonArray(jsonPrimitive(K1), jsonPrimitive(K2)),
+				is(stringJsonArray(K1, K2)));
+		assertThat(
+				jsonArray(null, null),
+				is(stringJsonArray(null, null))
+		);
+	}
+
+	@Test
+	public void testJsonArray3() {
+		assertThat(
+				jsonArray(jsonPrimitive(K1), jsonPrimitive(K2), jsonPrimitive(K3)),
+				is(stringJsonArray(K1, K2, K3)));
+		assertThat(
+				jsonArray(null, null, null),
+				is(stringJsonArray(null, null, null))
+		);
+	}
+
+	@Test
+	public void testJsonArray4() {
+		assertThat(
+				jsonArray(jsonPrimitive(K1), jsonPrimitive(K2), jsonPrimitive(K3), jsonPrimitive(K4)),
+				is(stringJsonArray(K1, K2, K3, K4)));
+		assertThat(
+				jsonArray(null, null, null, null),
+				is(stringJsonArray(null, null, null, null))
+		);
+	}
+
+	@Test
+	public void testJsonArray5() {
+		assertThat(
+				jsonArray(jsonPrimitive(K1), jsonPrimitive(K2), jsonPrimitive(K3), jsonPrimitive(K4), jsonPrimitive(K5)),
+				is(stringJsonArray(K1, K2, K3, K4, K5)));
+		assertThat(
+				jsonArray(null, null, null, null, null),
+				is(stringJsonArray(null, null, null, null, null))
+		);
+	}
+
+	@Test
+	public void testJsonArray6() {
+		assertThat(
+				jsonArray(jsonPrimitive(K1), jsonPrimitive(K2), jsonPrimitive(K3), jsonPrimitive(K4), jsonPrimitive(K5), jsonPrimitive(K6)),
+				is(stringJsonArray(K1, K2, K3, K4, K5, K6)));
+		assertThat(
+				jsonArray(null, null, null, null, null, null),
+				is(stringJsonArray(null, null, null, null, null, null))
+		);
+	}
+
+	@Test
+	public void testJsonArray7() {
+		assertThat(
+				jsonArray(jsonPrimitive(K1), jsonPrimitive(K2), jsonPrimitive(K3), jsonPrimitive(K4), jsonPrimitive(K5), jsonPrimitive(K6), jsonPrimitive(K7)),
+				is(stringJsonArray(K1, K2, K3, K4, K5, K6, K7)));
+		assertThat(
+				jsonArray(null, null, null, null, null, null, null),
+				is(stringJsonArray(null, null, null, null, null, null, null))
+		);
+	}
+
+	@Test
+	public void testJsonArray8() {
+		assertThat(
+				jsonArray(jsonPrimitive(K1), jsonPrimitive(K2), jsonPrimitive(K3), jsonPrimitive(K4), jsonPrimitive(K5), jsonPrimitive(K6), jsonPrimitive(K7), jsonPrimitive(K8)),
+				is(stringJsonArray(K1, K2, K3, K4, K5, K6, K7, K8)));
+		assertThat(
+				jsonArray(null, null, null, null, null, null, null, null),
+				is(stringJsonArray(null, null, null, null, null, null, null, null))
+		);
+	}
+
+	@Test
+	public void testJsonArray9() {
+		assertThat(
+				jsonArray(jsonPrimitive(K1), jsonPrimitive(K2), jsonPrimitive(K3), jsonPrimitive(K4), jsonPrimitive(K5), jsonPrimitive(K6), jsonPrimitive(K7), jsonPrimitive(K8), jsonPrimitive(K9)),
+				is(stringJsonArray(K1, K2, K3, K4, K5, K6, K7, K8, K9)));
+		assertThat(
+				jsonArray(null, null, null, null, null, null, null, null, null),
+				is(stringJsonArray(null, null, null, null, null, null, null, null, null))
+		);
+	}
+
+	@Test
+	public void testJsonArray10() {
+		assertThat(
+				jsonArray(jsonPrimitive(K1), jsonPrimitive(K2), jsonPrimitive(K3), jsonPrimitive(K4), jsonPrimitive(K5), jsonPrimitive(K6), jsonPrimitive(K7), jsonPrimitive(K8), jsonPrimitive(K9), jsonPrimitive(K10)),
+				is(stringJsonArray(K1, K2, K3, K4, K5, K6, K7, K8, K9, K10)));
+		assertThat(
+				jsonArray(null, null, null, null, null, null, null, null, null, null),
+				is(stringJsonArray(null, null, null, null, null, null, null, null, null, null))
+		);
+	}
 
 	@Test
 	public void testMergeIntoNewWithDefaultStrategy() {
@@ -137,6 +434,14 @@ public final class JsonElementsTest {
 
 	private static Matcher<JsonObject> hasPropertiesOf(final JsonElement v1, final JsonElement v2, final JsonElement v3) {
 		return allOf(hasProperty(K1, v1), hasProperty(K2, v2), hasProperty(K3, v3));
+	}
+
+	private static JsonArray stringJsonArray(final String... values) {
+		final JsonArray array = new JsonArray();
+		for ( final String value : values ) {
+			array.add(value);
+		}
+		return array;
 	}
 
 }
