cfef33d7811fd3038a443644155fcc61291f6094
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2018-04-19 17:50:55 +0300

    Multimaps can support arbitrary multimaps while deserialization

--- src/main/java/lsh/ext/gson/adapters/guava/MultimapTypeAdapter.java
+++ src/main/java/lsh/ext/gson/adapters/guava/MultimapTypeAdapter.java
@@ -3,6 +3,7 @@ package lsh.ext.gson.adapters.guava;
 import java.io.IOException;
 import java.util.Map.Entry;
 
+import com.google.common.base.Supplier;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
 import com.google.gson.TypeAdapter;
@@ -11,7 +12,7 @@ import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 
 /**
- * Represents a type adapter for {@link Multimap} from Google Guava. Multimaps are deserialized to {@link ArrayListMultimap}.
+ * Represents a type adapter for {@link Multimap} from Google Guava.
  *
  * @author Lyubomyr Shaydariv
  * @see MultimapTypeAdapterFactory
@@ -21,21 +22,39 @@ public final class MultimapTypeAdapter<V>
 		extends TypeAdapter<Multimap<String, V>> {
 
 	private final TypeAdapter<V> valueTypeAdapter;
+	private final Supplier<? extends Multimap<String, V>> newMultimapFactory;
 
-	private MultimapTypeAdapter(final TypeAdapter<V> valueTypeAdapter) {
+	private MultimapTypeAdapter(final TypeAdapter<V> valueTypeAdapter, final Supplier<? extends Multimap<String, V>> newMultimapFactory) {
 		this.valueTypeAdapter = valueTypeAdapter;
+		this.newMultimapFactory = newMultimapFactory;
 	}
 
 	/**
 	 * @param valueTypeAdapter Multimap value type adapter
 	 * @param <V>              Multimap value type parameter
 	 *
-	 * @return A {@link MultimapTypeAdapter} instance.
+	 * @return A {@link MultimapTypeAdapter} instance whose multimap factory is {@link ArrayListMultimap#create()}.
 	 *
+	 * @see #get(TypeAdapter, Supplier)
 	 * @since 0-SNAPSHOT
 	 */
 	public static <V> TypeAdapter<Multimap<String, V>> get(final TypeAdapter<V> valueTypeAdapter) {
-		return new MultimapTypeAdapter<>(valueTypeAdapter)
+		return get(valueTypeAdapter, (Supplier<? extends Multimap<String, V>>) ArrayListMultimap::create);
+	}
+
+	/**
+	 * @param valueTypeAdapter   Multimap value type adapter
+	 * @param newMultimapFactory A {@link Multimap} factory to create instance used while deserialization
+	 * @param <V>                Multimap value type parameter
+	 *
+	 * @return A {@link MultimapTypeAdapter} instance.
+	 *
+	 * @see #get(TypeAdapter)
+	 * @since 0-SNAPSHOT
+	 */
+	public static <V> TypeAdapter<Multimap<String, V>> get(final TypeAdapter<V> valueTypeAdapter,
+			final Supplier<? extends Multimap<String, V>> newMultimapFactory) {
+		return new MultimapTypeAdapter<>(valueTypeAdapter, newMultimapFactory)
 				.nullSafe();
 	}
 
@@ -56,7 +75,7 @@ public final class MultimapTypeAdapter<V>
 	@Override
 	public Multimap<String, V> read(final JsonReader in)
 			throws IOException {
-		final Multimap<String, V> multimap = ArrayListMultimap.create();
+		final Multimap<String, V> multimap = newMultimapFactory.get();
 		in.beginObject();
 		while ( in.peek() != JsonToken.END_OBJECT ) {
 			final String key = in.nextName();
