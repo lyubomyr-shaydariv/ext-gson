e6dc1825e16f79148263ecba245a4e5c138f3528
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2018-04-20 09:06:24 +0300

    Reworked AlwaysListTypeAdapterTest

--- src/test/java/lsh/ext/gson/adapters/AlwaysListTypeAdapterFactoryTest.java
+++ src/test/java/lsh/ext/gson/adapters/AlwaysListTypeAdapterFactoryTest.java
@@ -1,95 +1,35 @@
 package lsh.ext.gson.adapters;
 
-import java.io.IOException;
 import java.util.List;
-import java.util.Map;
+import javax.annotation.Nonnull;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.gson.Gson;
-import com.google.gson.TypeAdapter;
 import com.google.gson.TypeAdapterFactory;
 import com.google.gson.reflect.TypeToken;
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.Matcher;
-import org.hamcrest.MatcherAssert;
-import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-public final class AlwaysListTypeAdapterFactoryTest {
+@RunWith(Parameterized.class)
+public final class AlwaysListTypeAdapterFactoryTest
+		extends AbstractTypeAdapterFactoryTest {
 
-	private static final TypeToken<List<Boolean>> booleanListTypeToken = new TypeToken<List<Boolean>>() {};
-	private static final TypeToken<List<Integer>> integerListTypeToken = new TypeToken<List<Integer>>() {};
-	private static final TypeToken<List<String>> stringListTypeToken = new TypeToken<List<String>>() {};
-	private static final TypeToken<List<Map<String, Integer>>> stringToIntegerMapListTypeToken = new TypeToken<List<Map<String, Integer>>>() {};
-
-	private static final Gson gson = new Gson();
-	private static final TypeAdapterFactory unit = AlwaysListTypeAdapterFactory.get();
-
-	@Test
-	public void testNull()
-			throws IOException {
-		doTest(stringListTypeToken, "null", CoreMatchers.nullValue());
-	}
-
-	@Test
-	public void testSingleObject()
-			throws IOException {
-		doTest(stringToIntegerMapListTypeToken, "{\"foo\":1,\"bar\":2}", CoreMatchers.is(ImmutableList.of(ImmutableMap.of("foo", 1, "bar", 2))));
-	}
-
-	@Test
-	public void testSingleString()
-			throws IOException {
-		doTest(stringListTypeToken, "\"foo\"", CoreMatchers.is(ImmutableList.of("foo")));
-	}
-
-	@Test
-	public void testSingleNumber()
-			throws IOException {
-		doTest(integerListTypeToken, "39", CoreMatchers.is(ImmutableList.of(39)));
-	}
-
-	@Test
-	public void testSingleBoolean()
-			throws IOException {
-		doTest(booleanListTypeToken, "true", CoreMatchers.is(ImmutableList.of(true)));
-	}
-
-	@Test
-	public void testMultipleObjects()
-			throws IOException {
-		doTest(
-				stringToIntegerMapListTypeToken,
-				"[{\"foo\":1,\"bar\":2},{\"bar\":3,\"qux\":4}]",
-				CoreMatchers.is(ImmutableList.of(ImmutableMap.of("foo", 1, "bar", 2), ImmutableMap.of("bar", 3, "qux", 4)))
+	@Parameterized.Parameters
+	public static Iterable<TestWith> parameters() {
+		return ImmutableList.of(
+				testWith(
+						TypeToken.get(List.class)
+				)
 		);
 	}
 
-	@Test
-	public void testMultipleStrings()
-			throws IOException {
-		doTest(stringListTypeToken, "[\"foo\",\"bar\",\"baz\"]", CoreMatchers.is(ImmutableList.of("foo", "bar", "baz")));
+	public AlwaysListTypeAdapterFactoryTest(final TestWith testWith) {
+		super(testWith);
 	}
 
-	@Test
-	public void testMultipleNumbers()
-			throws IOException {
-		doTest(integerListTypeToken, "[39,42,100]", CoreMatchers.is(ImmutableList.of(39, 42, 100)));
-	}
-
-	@Test
-	public void testMultipleBooleans()
-			throws IOException {
-		doTest(booleanListTypeToken, "[true,false,true]", CoreMatchers.is(ImmutableList.of(true, false, true)));
-	}
-
-	private static <T> void doTest(final TypeToken<T> typeToken, final String json, final Matcher<? super T> valueMatcher)
-			throws IOException {
-		final TypeAdapter<T> typeAdapter = unit.create(gson, typeToken);
-		final T value = typeAdapter.fromJson(json);
-		MatcherAssert.assertThat(value, valueMatcher);
-		final String serializedJson = typeAdapter.toJson(value);
-		MatcherAssert.assertThat(serializedJson, CoreMatchers.is(json));
+	@Nonnull
+	@Override
+	protected TypeAdapterFactory createUnit() {
+		return AlwaysListTypeAdapterFactory.get();
 	}
 
 }
--- src/test/java/lsh/ext/gson/adapters/AlwaysListTypeAdapterTest.java
+++ src/test/java/lsh/ext/gson/adapters/AlwaysListTypeAdapterTest.java
@@ -1,93 +1,82 @@
 package lsh.ext.gson.adapters;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
 import com.google.gson.reflect.TypeToken;
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.Matcher;
-import org.hamcrest.MatcherAssert;
-import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-public final class AlwaysListTypeAdapterTest {
+@RunWith(Parameterized.class)
+public final class AlwaysListTypeAdapterTest
+		extends AbstractTypeAdapterTest<List<?>> {
 
-	private static final TypeToken<Map<String, Integer>> stringToIntegerMapTypeToken = new TypeToken<Map<String, Integer>>() {};
-
-	private static final Gson gson = new Gson();
-
-	@Test
-	public void testNull()
-			throws IOException {
-		doTest(createUnit(TypeToken.get(String.class)), "null", CoreMatchers.nullValue());
-	}
-
-	@Test
-	public void testSingleObject()
-			throws IOException {
-		doTest(createUnit(stringToIntegerMapTypeToken), "{\"foo\":1,\"bar\":2}", CoreMatchers.is(ImmutableList.of(ImmutableMap.of("foo", 1, "bar", 2))));
-	}
-
-	@Test
-	public void testSingleString()
-			throws IOException {
-		doTest(createUnit(TypeToken.get(String.class)), "\"foo\"", CoreMatchers.is(ImmutableList.of("foo")));
-	}
-
-	@Test
-	public void testSingleNumber()
-			throws IOException {
-		doTest(createUnit(TypeToken.get(Integer.class)), "39", CoreMatchers.is(ImmutableList.of(39)));
-	}
-
-	@Test
-	public void testSingleBoolean()
-			throws IOException {
-		doTest(createUnit(TypeToken.get(Boolean.class)), "true", CoreMatchers.is(ImmutableList.of(true)));
-	}
-
-	@Test
-	public void testMultipleObjects()
-			throws IOException {
-		doTest(
-				createUnit(stringToIntegerMapTypeToken),
-				"[{\"foo\":1,\"bar\":2},{\"bar\":3,\"qux\":4}]",
-				CoreMatchers.is(ImmutableList.of(ImmutableMap.of("foo", 1, "bar", 2), ImmutableMap.of("bar", 3, "qux", 4)))
+	@Parameterized.Parameters
+	public static Iterable<TestWith<? extends List<?>>> parameters() {
+		return ImmutableList.of(
+				testWith(
+						new TypeToken<Map<String, Integer>>() {},
+						ImmutableList.of(ImmutableMap.of("foo", 1, "bar", 2)),
+						"{\"foo\":1,\"bar\":2}"
+				),
+				testWith(
+						TypeToken.get(String.class),
+						ImmutableList.of("foo"),
+						"\"foo\""
+				),
+				testWith(
+						TypeToken.get(Integer.class),
+						ImmutableList.of(39),
+						"39"
+				),
+				testWith(
+						TypeToken.get(Boolean.class),
+						ImmutableList.of(true),
+						"true"
+				),
+				testWith(
+						new TypeToken<Map<String, Integer>>() {},
+						ImmutableList.of(ImmutableMap.of("foo", 1, "bar", 2), ImmutableMap.of("bar", 3, "qux", 4)),
+						"[{\"foo\":1,\"bar\":2},{\"bar\":3,\"qux\":4}]"
+				),
+				testWith(
+						TypeToken.get(String.class),
+						ImmutableList.of("foo", "bar", "baz"),
+						"[\"foo\",\"bar\",\"baz\"]"
+				),
+				testWith(
+						TypeToken.get(Integer.class),
+						ImmutableList.of(39, 42, 100),
+						"[39,42,100]"
+				),
+				testWith(
+						TypeToken.get(Boolean.class),
+						ImmutableList.of(true, false, true),
+						"[true,false,true]"
+				)
 		);
 	}
 
-	@Test
-	public void testMultipleStrings()
-			throws IOException {
-		doTest(createUnit(TypeToken.get(String.class)), "[\"foo\",\"bar\",\"baz\"]", CoreMatchers.is(ImmutableList.of("foo", "bar", "baz")));
+	public AlwaysListTypeAdapterTest(final TestWith<? extends List<?>> testWith) {
+		super(null, testWith);
 	}
 
-	@Test
-	public void testMultipleNumbers()
-			throws IOException {
-		doTest(createUnit(TypeToken.get(Integer.class)), "[39,42,100]", CoreMatchers.is(ImmutableList.of(39, 42, 100)));
-	}
-
-	@Test
-	public void testMultipleBooleans()
-			throws IOException {
-		doTest(createUnit(TypeToken.get(Boolean.class)), "[true,false,true]", CoreMatchers.is(ImmutableList.of(true, false, true)));
-	}
-
-	private static <T> TypeAdapter<List<T>> createUnit(final TypeToken<T> typeToken) {
+	@Nonnull
+	@Override
+	protected TypeAdapter<? extends List<?>> createUnit(@Nonnull final Gson gson, @Nullable final TypeToken<?> typeToken) {
 		return AlwaysListTypeAdapter.get(gson.getAdapter(typeToken));
 	}
 
-	private static <T> void doTest(final TypeAdapter<T> unit, final String json, final Matcher<? super T> valueMatcher)
-			throws IOException {
-		final T value = unit.fromJson(json);
-		MatcherAssert.assertThat(value, valueMatcher);
-		final String serializedJson = unit.toJson(value);
-		MatcherAssert.assertThat(serializedJson, CoreMatchers.is(json));
+	@Nonnull
+	@Override
+	protected Object finalizeValue(@Nonnull final List<?> value) {
+		return value;
 	}
 
 }
