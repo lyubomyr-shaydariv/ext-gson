eebc04780d05e80afb8c7591d5fef03be3655f9e
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2022-01-28 18:33:08 +0200

    Added GsonBuilders Javadocs

--- src/main/java/lsh/ext/gson/GsonBuilders.java
+++ src/main/java/lsh/ext/gson/GsonBuilders.java
@@ -4,13 +4,20 @@ import com.google.gson.GsonBuilder;
 import lombok.experimental.UtilityClass;
 
 /**
- * TODO
+ * Contains methods to maintain {@link GsonBuilder} instances.
+ *
+ * @author Lyubomyr Shaydariv
  */
 @UtilityClass
 public final class GsonBuilders {
 
 	/**
-	 * TODO
+	 * Creates a new "canonical" {@link GsonBuilder} instance excluding some default options from the default instance:
+	 *
+	 * <ul>
+	 *     <li>{@link GsonBuilder#disableInnerClassSerialization()}</li>
+	 *     <li>{@link GsonBuilder#disableHtmlEscaping()}</li>
+	 * </ul>
 	 */
 	public static GsonBuilder createCanonical() {
 		return new GsonBuilder()
