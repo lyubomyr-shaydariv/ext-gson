9bc49adadffc3da3d599ed9021fd4b6e72447e0a
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2023-01-04 22:48:31 +0200

    Removed redundant overloads

--- src/main/java/lsh/ext/gson/Json.java
+++ src/main/java/lsh/ext/gson/Json.java
@@ -1,8 +1,6 @@
 package lsh.ext.gson;
 
 import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
 
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonToken;
@@ -16,30 +14,6 @@ import lombok.experimental.UtilityClass;
  */
 @UtilityClass
 public final class Json {
-
-	/**
-	 * @param json JSON string
-	 *
-	 * @return {@code true} if the given JSON reader represents a valid JSON, otherwise {@code false}.
-	 *
-	 * @throws IOException A rethrown exception.
-	 */
-	public static boolean isValid(final String json)
-			throws IOException {
-		return isValid(new StringReader(json));
-	}
-
-	/**
-	 * @param reader Reader
-	 *
-	 * @return {@code true} if the given JSON reader represents a valid JSON, otherwise {@code false}.
-	 *
-	 * @throws IOException A rethrown exception
-	 */
-	public static boolean isValid(final Reader reader)
-			throws IOException {
-		return isValid(new JsonReader(reader));
-	}
 
 	/**
 	 * @param jsonReader JSON reader
--- src/test/java/lsh/ext/gson/JsonTest.java
+++ src/test/java/lsh/ext/gson/JsonTest.java
@@ -13,21 +13,7 @@ public final class JsonTest {
 	private static final String INVALID_JSON = "foobar";
 
 	@Test
-	public void isJsonValidForString()
-			throws IOException {
-		Assertions.assertTrue(Json.isValid(VALID_JSON));
-		Assertions.assertFalse(Json.isValid(INVALID_JSON));
-	}
-
-	@Test
-	public void isJsonValid1ForReader()
-			throws IOException {
-		Assertions.assertTrue(Json.isValid(new StringReader(VALID_JSON)));
-		Assertions.assertFalse(Json.isValid(new StringReader(INVALID_JSON)));
-	}
-
-	@Test
-	public void isJsonValid2ForJsonReader()
+	public void isJsonValid()
 			throws IOException {
 		Assertions.assertTrue(Json.isValid(new JsonReader(new StringReader(VALID_JSON))));
 		Assertions.assertFalse(Json.isValid(new JsonReader(new StringReader(INVALID_JSON))));
