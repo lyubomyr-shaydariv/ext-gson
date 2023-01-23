49561cf66e6088b54a24e14e630344ffe233eb0e
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2023-01-23 23:27:54 +0200

    How should this be properly handled?

--- src/main/java/lsh/ext/gson/ext/jakarta/json/package-info.java
+++ src/main/java/lsh/ext/gson/ext/jakarta/json/package-info.java
@@ -1,3 +1,8 @@
+// TODO Somehow consider 100%-copying this package from javax.json
+// Available options:
+// - A pure OOP approach (requires multiple (+abstract) classes, complicates design by exploding the number of required types)
+// - A codegen for generate-sources?
+// - A post-compilation processing for the class files (the maven-shade-plugin does not seem to be an option here)?
 @NonnullByDefault
 package lsh.ext.gson.ext.jakarta.json;
 
