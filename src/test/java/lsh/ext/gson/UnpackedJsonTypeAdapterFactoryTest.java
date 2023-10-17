37334953fa02008a435df95d7dc174098507916b
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2023-10-17 14:33:32 +0300

    Removed intermediate abstract test class

--- src/test/java/lsh/ext/gson/UnpackedJsonTypeAdapterFactoryTest.java
+++ src/test/java/lsh/ext/gson/UnpackedJsonTypeAdapterFactoryTest.java
@@ -1,25 +1,30 @@
 package lsh.ext.gson;
 
 import java.util.List;
-import java.util.stream.Stream;
 
+import com.google.gson.Gson;
 import com.google.gson.annotations.JsonAdapter;
-import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-public final class UnpackedJsonTypeAdapterFactoryTest
-		extends AbstractSimpleTest {
+public final class UnpackedJsonTypeAdapterFactoryTest {
+
+	private static final Gson gson = Gsons.getNormalized();
+
+	@Test
+	public void testRead() {
+		Assertions.assertEquals(new Wrapper(List.of(1, 2, 3)), gson.fromJson("{\"value\":\"[1,2,3]\"}", Wrapper.class));
+	}
+
+	@Test
+	public void testWrite() {
+		Assertions.assertEquals("{\"value\":\"[1,3]\"}", gson.toJson(new Wrapper(List.of(1, 3)), Wrapper.class));
+	}
 
 	private record Wrapper(
 			@JsonAdapter(UnpackedJsonTypeAdapterFactory.class)
 			List<Integer> value
 	) {
-	}
-
-	@Override
-	protected Stream<Arguments> source() {
-		return Stream.of(
-				Arguments.of(Wrapper.class, "{\"value\":\"[1,2,3]\"}", new Wrapper(List.of(1, 2, 3)))
-		);
 	}
 
 }
