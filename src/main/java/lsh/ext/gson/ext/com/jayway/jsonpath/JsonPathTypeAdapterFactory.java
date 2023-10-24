2975d6a635f4578b645ecbebfe8f9eb2a4f90b69
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2023-10-25 01:42:54 +0300

    Minor changes to the JsonPath type adapter factory

--- src/main/java/lsh/ext/gson/ext/com/jayway/jsonpath/JsonPathTypeAdapterFactory.java
+++ src/main/java/lsh/ext/gson/ext/com/jayway/jsonpath/JsonPathTypeAdapterFactory.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.function.Function;
 import javax.annotation.Nullable;
 
@@ -127,10 +126,12 @@ public final class JsonPathTypeAdapterFactory
 	@Override
 	public <T> TypeAdapter<T> create(final Gson gson, final TypeToken<T> typeToken) {
 		final TypeAdapter<T> delegateAdapter = gson.getDelegateAdapter(this, typeToken);
-		final Collection<FieldInfo> fieldInfos = FieldInfo.of(typeToken.getRawType(), gson);
-		return fieldInfos.isEmpty()
-				? delegateAdapter
-				: new JsonPathTypeAdapter<>(delegateAdapter, gson.getAdapter(JsonElement.class), fieldInfos, configurationProvider.apply(gson));
+		@Nullable
+		final Collection<FieldDatum> fieldData = findFields(typeToken.getRawType(), gson);
+		if ( fieldData == null ) {
+			return delegateAdapter;
+		}
+		return new Adapter<>(delegateAdapter, gson.getAdapter(JsonElement.class), fieldData, configurationProvider.apply(gson));
 	}
 
 	private static Configuration buildDefaultConfiguration(final Gson gson) {
@@ -140,13 +141,33 @@ public final class JsonPathTypeAdapterFactory
 				.build();
 	}
 
+	@Nullable
+	private static Collection<FieldDatum> findFields(final Class<?> clazz, final Gson gson) {
+		@Nullable
+		Collection<FieldDatum> collection = null;
+		for ( final Field field : clazz.getDeclaredFields() ) {
+			@Nullable
+			final JsonPathExpression jsonPathExpression = field.getAnnotation(JsonPathExpression.class);
+			if ( jsonPathExpression == null ) {
+				continue;
+			}
+			if ( collection == null ) {
+				collection = new ArrayList<>();
+			}
+			field.setAccessible(true);
+			final TypeAdapter<?> typeAdapter = gson.getAdapter(TypeToken.get(field.getType()));
+			collection.add(new FieldDatum(field, JsonPath.compile(jsonPathExpression.value()), typeAdapter));
+		}
+		return collection;
+	}
+
 	@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
-	private static final class JsonPathTypeAdapter<T>
+	private static final class Adapter<T>
 			extends TypeAdapter<T> {
 
 		private final TypeAdapter<T> delegateAdapter;
-		private final TypeAdapter<JsonElement> jsonElementTypeAdapter;
-		private final Collection<FieldInfo> fieldInfos;
+		private final TypeAdapter<? extends JsonElement> jsonElementTypeAdapter;
+		private final Collection<FieldDatum> fieldData;
 		private final Configuration configuration;
 
 		@Override
@@ -160,11 +181,11 @@ public final class JsonPathTypeAdapterFactory
 				throws IOException {
 			final JsonElement outerJsonElement = jsonElementTypeAdapter.read(in);
 			final T value = delegateAdapter.fromJsonTree(outerJsonElement);
-			for ( final FieldInfo fieldInfo : fieldInfos ) {
+			for ( final FieldDatum fieldDatum : fieldData ) {
 				try {
-					final JsonElement innerJsonElement = fieldInfo.jsonPath.read(outerJsonElement, configuration);
-					final Object innerValue = fieldInfo.typeAdapter.fromJsonTree(innerJsonElement);
-					fieldInfo.field.set(value, innerValue);
+					final JsonElement innerJsonElement = fieldDatum.jsonPath.read(outerJsonElement, configuration);
+					final Object innerValue = fieldDatum.typeAdapter.fromJsonTree(innerJsonElement);
+					fieldDatum.field.set(value, innerValue);
 				} catch ( final PathNotFoundException ignored ) {
 					// do nothing
 				} catch ( final IllegalAccessException ex ) {
@@ -177,29 +198,11 @@ public final class JsonPathTypeAdapterFactory
 	}
 
 	@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
-	private static final class FieldInfo {
+	private static final class FieldDatum {
 
 		private final Field field;
 		private final JsonPath jsonPath;
 		private final TypeAdapter<?> typeAdapter;
-
-		private static Collection<FieldInfo> of(final Class<?> clazz, final Gson gson) {
-			Collection<FieldInfo> collection = Collections.emptyList();
-			for ( final Field field : clazz.getDeclaredFields() ) {
-				@Nullable
-				final JsonPathExpression jsonPathExpression = field.getAnnotation(JsonPathExpression.class);
-				if ( jsonPathExpression == null ) {
-					continue;
-				}
-				if ( collection.isEmpty() ) {
-					collection = new ArrayList<>();
-				}
-				field.setAccessible(true);
-				final TypeAdapter<?> typeAdapter = gson.getAdapter(TypeToken.get(field.getType()));
-				collection.add(new FieldInfo(field, JsonPath.compile(jsonPathExpression.value()), typeAdapter));
-			}
-			return collection;
-		}
 
 	}
 
--- src/test/java/lsh/ext/gson/ext/com/jayway/jsonpath/JsonPathTypeAdapterTest.java
+++ src/test/java/lsh/ext/gson/ext/com/jayway/jsonpath/JsonPathTypeAdapterTest.java
@@ -7,9 +7,6 @@ import javax.annotation.Nullable;
 import com.google.gson.Gson;
 import com.google.gson.TypeAdapterFactory;
 import com.google.gson.reflect.TypeToken;
-import com.jayway.jsonpath.Configuration;
-import com.jayway.jsonpath.spi.json.GsonJsonProvider;
-import com.jayway.jsonpath.spi.mapper.GsonMappingProvider;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
@@ -31,7 +28,7 @@ public final class JsonPathTypeAdapterTest
 
 	@Override
 	protected List<Arguments> makeTestCases() {
-		final TypeAdapterFactory typeAdapterFactory = JsonPathTypeAdapterFactory.getInstance(JsonPathTypeAdapterTest::getJsonPathConfiguration);
+		final TypeAdapterFactory typeAdapterFactory = JsonPathTypeAdapterFactory.getInstance();
 		return List.of(
 				makeTestCase(
 						typeAdapterFactory.create(gson, TypeToken.get(Wrapper.class)),
@@ -46,7 +43,7 @@ public final class JsonPathTypeAdapterTest
 						new WrapperWithNotExistingPath(null)
 				),
 				makeTestCase(
-						JsonPathTypeAdapterFactory.getInstance(JsonPathTypeAdapterTest::getJsonPathConfiguration).create(gson, TypeToken.get(WrapperWithNotExistingPath.class)),
+						JsonPathTypeAdapterFactory.getInstance().create(gson, TypeToken.get(WrapperWithNotExistingPath.class)),
 						"{\"l1\":{\"l2\":{\"l3\":{\"foo\":\"Foo!\",\"bar\":[\"A\",\"B\",\"C\"],\"baz\":{\"k1\":\"v1\"}}}}}",
 						"{\"fooRef\":null}",
 						new WrapperWithNotExistingPath(null)
@@ -58,13 +55,6 @@ public final class JsonPathTypeAdapterTest
 						new WrapperWithNotExistingPath(null)
 				)
 		);
-	}
-
-	private static Configuration getJsonPathConfiguration(final Gson gson) {
-		return Configuration.builder()
-				.jsonProvider(new GsonJsonProvider(gson))
-				.mappingProvider(new GsonMappingProvider(gson))
-				.build();
 	}
 
 	@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
