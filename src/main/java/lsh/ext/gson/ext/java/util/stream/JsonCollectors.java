7d6c386bc631f936e6e87b4bf87e1e8b29323c76
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2023-10-30 20:17:25 +0200

    Fixed JSON collectors

--- src/main/java/lsh/ext/gson/ext/java/util/stream/JsonCollectors.java
+++ src/main/java/lsh/ext/gson/ext/java/util/stream/JsonCollectors.java
@@ -12,46 +12,34 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public final class JsonCollectors {
 
-	private static final Collector<Map.Entry<String, ? extends JsonElement>, JsonObject, JsonObject> toNewJsonObject = createToJsonObject(JsonObject::new);
-	private static final Collector<JsonElement, JsonArray, JsonArray> toNewJsonArray = createToJsonArray(JsonArray::new);
-
-	public static Collector<Map.Entry<String, ? extends JsonElement>, JsonObject, JsonObject> toJsonObject() {
-		return toNewJsonObject;
+	public static Collector<Map.Entry<String, ? extends JsonElement>, ?, JsonObject> toJsonObject() {
+		return toJsonObject(JsonObject::new);
 	}
 
-	public static Collector<Map.Entry<String, ? extends JsonElement>, JsonObject, JsonObject> toJsonObject(final Supplier<JsonObject> jsonObjectSupplier) {
-		return createToJsonObject(jsonObjectSupplier);
-	}
-
-	public static Collector<JsonElement, JsonArray, JsonArray> toJsonArray() {
-		return toNewJsonArray;
-	}
-
-	public static Collector<JsonElement, JsonArray, JsonArray> toJsonArray(final Supplier<JsonArray> jsonArraySupplier) {
-		return createToJsonArray(jsonArraySupplier);
-	}
-
-	private static Collector<Map.Entry<String, ? extends JsonElement>, JsonObject, JsonObject> createToJsonObject(
-			final Supplier<JsonObject> jsonObjectSupplier) {
+	public static Collector<Map.Entry<String, ? extends JsonElement>, ?, JsonObject> toJsonObject(final Supplier<JsonObject> jsonObjectSupplier) {
 		return Collector.of(
 				jsonObjectSupplier,
-				(jsonObject1, e) -> jsonObject1.add(e.getKey(), e.getValue()),
-				(jsonObject1, jsonObject2) -> {
-					for ( final Map.Entry<String, JsonElement> e : jsonObject2.entrySet() ) {
-						jsonObject1.add(e.getKey(), e.getValue());
+				(o, e) -> o.add(e.getKey(), e.getValue()),
+				(o1, o2) -> {
+					for ( final Map.Entry<String, JsonElement> e : o2.entrySet() ) {
+						o1.add(e.getKey(), e.getValue());
 					}
-					return jsonObject1;
+					return o1;
 				}
 		);
 	}
 
-	private static Collector<JsonElement, JsonArray, JsonArray> createToJsonArray(final Supplier<JsonArray> jsonArraySupplier) {
+	public static Collector<JsonElement, ?, JsonArray> toJsonArray() {
+		return toJsonArray(JsonArray::new);
+	}
+
+	public static Collector<JsonElement, ?, JsonArray> toJsonArray(final Supplier<JsonArray> jsonArraySupplier) {
 		return Collector.of(
 				jsonArraySupplier,
 				JsonArray::add,
-				(jsonArray1, jsonArray2) -> {
-					jsonArray1.addAll(jsonArray2);
-					return jsonArray1;
+				(o1, o2) -> {
+					o1.addAll(o2);
+					return o1;
 				}
 		);
 	}
