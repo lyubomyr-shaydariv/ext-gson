58a6c664cc8b5788b686afaa57a7cd9b42fdc17c
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-08-05 10:19:20 +0300

    Fused TypeEncoderTypeAdapter into AbstractEncodingTypeAdapter

--- src/main/java/lsh/ext/gson/AbstractEncodingTypeAdapter.java
+++ src/main/java/lsh/ext/gson/AbstractEncodingTypeAdapter.java
@@ -1,6 +1,7 @@
 package lsh.ext.gson;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 
 import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
@@ -8,7 +9,9 @@ import com.google.gson.TypeAdapterFactory;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
+import com.google.gson.stream.MalformedJsonException;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
@@ -85,6 +88,116 @@ public abstract class AbstractEncodingTypeAdapter<T, U>
 			public <T> TypeAdapter<T> create(final Gson gson, final TypeToken<T> typeToken) {
 				final TypeAdapter<T> typeAdapter = gson.getAdapter(typeToken);
 				return getInstance(typeAdapter);
+			}
+
+		}
+
+	}
+
+	@AllArgsConstructor(access = AccessLevel.PRIVATE)
+	public static final class JsonAsTypedValue<T, TT extends Type>
+			extends AbstractEncodingTypeAdapter<T, T> {
+
+		private final Gson gson;
+		private final String typePropertyName;
+		private final String valuePropertyName;
+		private final ITypeResolver<? extends TT> typeResolver;
+		private final ITypeEncoder<TT> typeEncoder;
+		private final ITypeEncoder.IGuard guard;
+
+		public static <T, TT extends Type> TypeAdapter<T> getInstance(
+				final Gson gson,
+				final String typePropertyName,
+				final String valuePropertyName,
+				final ITypeResolver<? extends TT> typeResolver,
+				final ITypeEncoder<TT> typeEncoder,
+				final ITypeEncoder.IGuard guard
+		) {
+			return new JsonAsTypedValue<T, TT>(gson, typePropertyName, valuePropertyName, typeResolver, typeEncoder, guard)
+					.nullSafe();
+		}
+
+		@Override
+		protected T encode(final T value) {
+			return value;
+		}
+
+		@Override
+		protected void writeEncoded(final JsonWriter out, final T value)
+				throws IOException {
+			out.beginObject();
+			out.name(typePropertyName);
+			final TT type = typeResolver.typeOf(value);
+			out.value(typeEncoder.encode(type));
+			out.name(valuePropertyName);
+			gson.toJson(value, type, out);
+			out.endObject();
+		}
+
+		@Override
+		protected T readEncoded(final JsonReader in)
+				throws IOException {
+			in.beginObject();
+			requireName(typePropertyName, in);
+			final String typeName = in.nextString();
+			if ( !guard.passes(typeName) ) {
+				throw new IOException("Type name " + typeName + " is not allowed");
+			}
+			requireName(valuePropertyName, in);
+			final T value = gson.fromJson(in, typeEncoder.decode(typeName));
+			in.endObject();
+			return value;
+		}
+
+		@Override
+		protected T decode(final T encodedValue) {
+			return encodedValue;
+		}
+
+		private static void requireName(final String expectedName, final JsonReader in)
+				throws IOException {
+			final String actualName = in.nextName();
+			if ( !actualName.equals(expectedName) ) {
+				throw new MalformedJsonException("Expected " + expectedName + " but was " + actualName + " at " + in.getPath());
+			}
+		}
+
+		public interface ITypeResolver<T extends Type> {
+
+			T typeOf(Object o);
+
+			ITypeResolver<Class<?>> forClass = Object::getClass;
+
+		}
+
+		public interface ITypeEncoder<T extends Type> {
+
+			String encode(T type);
+
+			T decode(String typeName);
+
+			ITypeEncoder<Class<?>> forUnsafeClass = new ITypeEncoder<>() {
+				@Override
+				public String encode(final Class<?> type) {
+					return type.getTypeName();
+				}
+
+				@Override
+				public Class<?> decode(final String typeName) {
+					try {
+						return Class.forName(typeName);
+					} catch ( final ClassNotFoundException ex ) {
+						throw new RuntimeException(ex);
+					}
+				}
+			};
+
+			interface IGuard {
+
+				boolean passes(String typeName);
+
+				IGuard weak = name -> true;
+
 			}
 
 		}
--- src/test/java/lsh/ext/gson/AbstractEncodingTypeAdapterFactoryTest.java
+++ src/test/java/lsh/ext/gson/AbstractEncodingTypeAdapterFactoryTest.java
@@ -1,11 +1,26 @@
 package lsh.ext.gson;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonIOException;
+import com.google.gson.TypeAdapter;
 import com.google.gson.annotations.JsonAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 
 public final class AbstractEncodingTypeAdapterFactoryTest {
 
@@ -25,5 +40,72 @@ public final class AbstractEncodingTypeAdapterFactoryTest {
 			@JsonAdapter(AbstractEncodingTypeAdapter.JsonAsString.Factory.class) List<Integer> value
 	) {
 	}
+
+	private static final AbstractEncodingTypeAdapter.JsonAsTypedValue.ITypeResolver<Class<?>> forClass = AbstractEncodingTypeAdapter.JsonAsTypedValue.ITypeResolver.forClass;
+	private static final AbstractEncodingTypeAdapter.JsonAsTypedValue.ITypeEncoder<Class<?>> forUnsafeClass = AbstractEncodingTypeAdapter.JsonAsTypedValue.ITypeEncoder.forUnsafeClass;
+	private static final AbstractEncodingTypeAdapter.JsonAsTypedValue.ITypeEncoder.IGuard weakGuard = AbstractEncodingTypeAdapter.JsonAsTypedValue.ITypeEncoder.IGuard.weak;
+
+	@Test
+	public void testJsonAsTypedValueReadWrite()
+			throws IOException {
+		final TypeAdapter<Integer> typeAdapter = AbstractEncodingTypeAdapter.JsonAsTypedValue.getInstance(gson, "type", "value", forClass, forUnsafeClass, weakGuard);
+		final Writer jsonWriter = new StringWriter();
+		final int before = 2;
+		typeAdapter.write(new JsonWriter(jsonWriter), before);
+		final String json = jsonWriter.toString();
+		final int after = typeAdapter.read(new JsonReader(new StringReader(json)));
+		Assertions.assertEquals(before, after);
+	}
+
+	@Test
+	public void testJsonAsTypedValueReadWriteGuardPasses() {
+		final AbstractEncodingTypeAdapter.JsonAsTypedValue.ITypeEncoder.IGuard primitiveGuardMock = Mockito.mock(AbstractEncodingTypeAdapter.JsonAsTypedValue.ITypeEncoder.IGuard.class);
+		Mockito.doAnswer(invocationOnMock -> primitiveTypeGuard.passes(invocationOnMock.getArgument(0, String.class)))
+				.when(primitiveGuardMock)
+				.passes(ArgumentMatchers.anyString());
+		final TypeAdapter<Integer> typeAdapter = AbstractEncodingTypeAdapter.JsonAsTypedValue.getInstance(gson, "t", "v", forClass, primitiveTypeDecoder, primitiveGuardMock);
+		final JsonElement legalJsonTree = JsonObjects.of(
+				"t", JsonPrimitives.orNullable("int"),
+				"v", JsonPrimitives.orNullable(1000)
+		);
+		final Integer actualPayload = Assertions.assertDoesNotThrow(() -> typeAdapter.fromJsonTree(legalJsonTree));
+		Assertions.assertEquals(1000, actualPayload);
+		Mockito.verify(primitiveGuardMock)
+				.passes(ArgumentMatchers.eq("int"));
+		Mockito.verifyNoMoreInteractions(primitiveGuardMock);
+	}
+
+	@Test
+	public void testJsonAsTypedValueReadWriteGuardDoesNotPass() {
+		final AbstractEncodingTypeAdapter.JsonAsTypedValue.ITypeEncoder.IGuard primitiveGuardMock = Mockito.mock(AbstractEncodingTypeAdapter.JsonAsTypedValue.ITypeEncoder.IGuard.class);
+		Mockito.doAnswer(invocationOnMock -> primitiveTypeGuard.passes(invocationOnMock.getArgument(0, String.class)))
+				.when(primitiveGuardMock)
+				.passes(ArgumentMatchers.anyString());
+		final TypeAdapter<Integer> typeAdapter = AbstractEncodingTypeAdapter.JsonAsTypedValue.getInstance(gson, "t", "v", forClass, primitiveTypeDecoder, primitiveGuardMock);
+		final JsonElement illegalJsonTree = JsonObjects.of(
+				"t", JsonPrimitives.orNullable("void")
+		);
+		Assertions.assertThrows(JsonIOException.class, () -> typeAdapter.fromJsonTree(illegalJsonTree));
+		Mockito.verify(primitiveGuardMock)
+				.passes(ArgumentMatchers.eq("void"));
+		Mockito.verifyNoMoreInteractions(primitiveGuardMock);
+	}
+
+	private static final Map<String, Class<?>> primitiveTypeNames = Stream.of(boolean.class, byte.class, short.class, int.class, long.class, float.class, double.class, char.class)
+			.collect(Collectors.toMap(Class::getName, Function.identity()));
+
+	private static final AbstractEncodingTypeAdapter.JsonAsTypedValue.ITypeEncoder.IGuard primitiveTypeGuard = primitiveTypeNames::containsKey;
+
+	private static final AbstractEncodingTypeAdapter.JsonAsTypedValue.ITypeEncoder<Class<?>> primitiveTypeDecoder = new AbstractEncodingTypeAdapter.JsonAsTypedValue.ITypeEncoder<>() {
+		@Override
+		public String encode(final Class<?> type) {
+			throw new AssertionError(type);
+		}
+
+		@Override
+		public Class<?> decode(final String typeName) {
+			return primitiveTypeNames.get(typeName);
+		}
+	};
 
 }
