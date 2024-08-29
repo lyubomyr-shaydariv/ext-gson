b17410309ef595b680f9a9081e3e81ef203a0d3e
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-08-29 22:15:51 +0300

    Added a sample iterator cursor test

--- src/test/java/lsh/ext/gson/ext/java/util/IteratorTypeAdapterTest.java
+++ src/test/java/lsh/ext/gson/ext/java/util/IteratorTypeAdapterTest.java
@@ -1,5 +1,7 @@
 package lsh.ext.gson.ext.java.util;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Spliterator;
@@ -8,8 +10,13 @@ import java.util.stream.StreamSupport;
 import javax.annotation.Nullable;
 
 import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
 import lsh.ext.gson.AbstractElementCursorTypeAdapterTest;
 import lsh.ext.gson.Gsons;
+import lsh.ext.gson.test.TypeAdapters;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.provider.Arguments;
 
 public final class IteratorTypeAdapterTest
@@ -32,6 +39,26 @@ public final class IteratorTypeAdapterTest
 						List.of(1, 2, 4, 8).iterator()
 				)
 		);
+	}
+
+	@Test
+	public void testLaziness()
+			throws IOException {
+		final TypeAdapter<Iterator<? extends Integer>> unit = IteratorTypeAdapter.getInstance(TypeAdapters.getIntTypeAdapter());
+		final JsonReader in = new JsonReader(new StringReader("[1,2,4,8]"));
+		final Iterator<? extends Integer> iterator = unit.read(in);
+		Assertions.assertEquals("$", in.getPath());
+		in.beginArray();
+		Assertions.assertEquals(1, iterator.next());
+		Assertions.assertEquals("$[1]", in.getPath());
+		Assertions.assertEquals(2, iterator.next());
+		Assertions.assertEquals("$[2]", in.getPath());
+		Assertions.assertEquals(4, iterator.next());
+		Assertions.assertEquals("$[3]", in.getPath());
+		Assertions.assertEquals(8, iterator.next());
+		Assertions.assertEquals("$[4]", in.getPath());
+		in.endArray();
+		Assertions.assertEquals("$", in.getPath());
 	}
 
 }
