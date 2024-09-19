2e1467310eb0b39bdc0ed1477f5fee1495dc55b8
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-09-19 12:52:09 +0300

    Implemented Map.Entry type adapter

---
+++ src/main/java/lsh/ext/gson/ext/java/util/MapEntryTypeAdapter.java
@@ -0,0 +1,60 @@
+package lsh.ext.gson.ext.java.util;
+
+import java.io.IOException;
+import java.util.AbstractMap;
+import java.util.Map;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lsh.ext.gson.IFunction;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public final class MapEntryTypeAdapter<K, V>
+		extends TypeAdapter<Map.Entry<K, V>> {
+
+	private final TypeAdapter<V> valueTypeAdapter;
+	private final IFunction<? super K, String> encodeKey;
+	private final IFunction<? super String, ? extends K> decodeKey;
+
+	public static <K, V> TypeAdapter<Map.Entry<K, V>> getInstance(
+			final TypeAdapter<V> valueTypeAdapter,
+			final IFunction<? super K, String> encodeKey,
+			final IFunction<? super String, ? extends K> decodeKey
+	) {
+		return new MapEntryTypeAdapter<K, V>(valueTypeAdapter, encodeKey, decodeKey)
+				.nullSafe();
+	}
+
+	@Override
+	public void write(final JsonWriter out, final Map.Entry<K, V> entry)
+			throws IOException {
+		out.beginObject();
+		out.name(encodeKey.apply(entry.getKey()));
+		valueTypeAdapter.write(out, entry.getValue());
+		out.endObject();
+	}
+
+	@Override
+	public Map.Entry<K, V> read(final JsonReader in)
+			throws IOException {
+		in.beginObject();
+		final Map.Entry<K, V> entry;
+		if ( !in.hasNext() ) {
+			entry = new AbstractMap.SimpleImmutableEntry<>(null, null); // TODO strategy!
+		} else {
+			final K k = decodeKey.apply(in.nextName());
+			final V v = valueTypeAdapter.read(in);
+			if ( in.hasNext() ) {
+				final String excessiveKey = in.nextName();
+				throw new IllegalStateException(String.format("Expected a single property object with key `%s` but also encountered `%s`", k, excessiveKey));
+			}
+			entry = new AbstractMap.SimpleImmutableEntry<>(k, v); // TODO strategy!
+		}
+		in.endObject();
+		return entry;
+	}
+
+}
---
+++ src/test/java/lsh/ext/gson/ext/java/util/MapEntryTypeAdapterTest.java
@@ -0,0 +1,52 @@
+package lsh.ext.gson.ext.java.util;
+
+import java.io.IOException;
+import java.util.Map;
+
+import com.google.gson.TypeAdapter;
+import lsh.ext.gson.IFunction;
+import lsh.ext.gson.test.MoreAssertions;
+import lsh.ext.gson.test.TypeAdapters;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public final class MapEntryTypeAdapterTest {
+
+	private static final TypeAdapter<Map.Entry<Double, Integer>> unit = MapEntryTypeAdapter.getInstance(
+			TypeAdapters.intTypeAdapter,
+			new IFunction<Double, String>() {
+				@Override
+				public String apply(final Double aDouble) {
+					return String.valueOf(aDouble);
+				}
+			},
+			new IFunction<String, Double>() {
+				@Override
+				public Double apply(final String s) {
+					return Double.parseDouble(s);
+				}
+			}
+	);
+
+	@Test
+	public void testReadForNoProperties()
+			throws IOException {
+		final Map.Entry<Double, Integer> e = unit.fromJson("{}");
+		Assertions.assertNull(e.getKey());
+		Assertions.assertNull(e.getValue());
+	}
+
+	@Test
+	public void testReadForSingleProperty()
+			throws IOException {
+		final Map.Entry<Double, Integer> e = unit.fromJson("{\"3.14\":1}");
+		Assertions.assertEquals(3.14D, e.getKey());
+		Assertions.assertEquals(1, e.getValue());
+	}
+
+	@Test
+	public void testReadForMultipleProperties() {
+		MoreAssertions.assertThrows(IllegalStateException.class, "Expected a single property object with key `3.14` but also encountered `2.7`", () -> unit.fromJson("{\"3.14\":1,\"2.7\":3,\"UNREACHABLE\":\"UNREACHABLE\"}"));
+	}
+
+}
