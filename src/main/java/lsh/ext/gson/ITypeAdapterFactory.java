98f5b3959de1649317bc47690a60f18a22cb3393
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-09-20 23:06:30 +0300

    Added a specialized factory method for constant instance type adapters

--- src/main/java/lsh/ext/gson/ITypeAdapterFactory.java
+++ src/main/java/lsh/ext/gson/ITypeAdapterFactory.java
@@ -15,4 +15,21 @@ public interface ITypeAdapterFactory<CT>
 	@Nullable
 	<T> TypeAdapter<T> create(Gson gson, TypeToken<T> typeToken);
 
+	static <CT> ITypeAdapterFactory<CT> forClass(final Class<CT> klass, final IFactory<? extends TypeAdapter<CT>> createTypeAdapter) {
+		return new ITypeAdapterFactory<>() {
+			@Nullable
+			@Override
+			public <T> TypeAdapter<T> create(final Gson gson, final TypeToken<T> typeToken) {
+				if ( typeToken.getRawType() != klass ) {
+					return null;
+				}
+				@SuppressWarnings("unchecked")
+				final TypeToken<CT> castTypeToken = (TypeToken<CT>) typeToken;
+				@SuppressWarnings("unchecked")
+				final TypeAdapter<T> castTypeAdapter = (TypeAdapter<T>) createTypeAdapter.create();
+				return castTypeAdapter;
+			}
+		};
+	}
+
 }
