66f20f45f62a42d41bf985f0d1b2a66640f134e1
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-09-20 20:30:41 +0300

    Added JSON string type adapter

---
+++ src/main/java/lsh/ext/gson/JsonStringTypeAdapter.java
@@ -0,0 +1,45 @@
+package lsh.ext.gson;
+
+import java.io.IOException;
+import javax.annotation.Nullable;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public final class JsonStringTypeAdapter<T>
+		extends TypeAdapter<T> {
+
+	private final IFunction1<? super T, String> toString;
+	private final IFunction1<? super String, ? extends T> fromString;
+
+	public static <T> TypeAdapter<T> getInstance(
+			final IFunction1<? super T, String> toString,
+			final IFunction1<? super String, ? extends T> fromString
+	) {
+		return new JsonStringTypeAdapter<T>(toString, fromString)
+				.nullSafe();
+	}
+
+	@Override
+	public void write(final JsonWriter out, final T value)
+			throws IOException {
+		@Nullable
+		final String s = toString.apply(value);
+		if ( s == null ) {
+			out.nullValue();
+			return;
+		}
+		out.value(s);
+	}
+
+	@Override
+	public T read(final JsonReader in)
+			throws IOException {
+		return fromString.apply(in.nextString());
+	}
+
+}
