af6385bb3d3db787b46cc456f92cf58651931586
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-09-21 10:37:19 +0300

    Added specialized type adapters for Base64 and URL

--- src/main/java/lsh/ext/gson/domain/encoded/EncodingTypeAdapter.java
+++ src/main/java/lsh/ext/gson/domain/encoded/EncodingTypeAdapter.java
@@ -1,12 +1,27 @@
 package lsh.ext.gson.domain.encoded;
 
+import java.util.Base64;
+
 import com.google.gson.TypeAdapter;
+import lombok.Getter;
 import lombok.experimental.UtilityClass;
 import lsh.ext.gson.IFunction1;
 import lsh.ext.gson.JsonStringTypeAdapter;
 
 @UtilityClass
 public final class EncodingTypeAdapter {
+
+	@Getter
+	private static final TypeAdapter<byte[]> defaultForPrimitiveByteArrayAsBase64 = forPrimitiveByteArray(
+			Base64.getEncoder()::encodeToString,
+			Base64.getDecoder()::decode
+	);
+
+	@Getter
+	private static final TypeAdapter<byte[]> defaultForPrimitiveByteArrayAsUrl = forPrimitiveByteArray(
+			Base64.getUrlEncoder()::encodeToString,
+			Base64.getUrlDecoder()::decode
+	);
 
 	public static TypeAdapter<byte[]> forPrimitiveByteArray(
 			final IFunction1<? super byte[], String> encode,
