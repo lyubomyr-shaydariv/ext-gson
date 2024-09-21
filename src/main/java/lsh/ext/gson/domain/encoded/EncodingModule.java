5925bd908bd7b070a311259cfa5913e5cc90a042
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-09-21 12:58:56 +0300

    Added the encoding module

---
+++ src/main/java/lsh/ext/gson/domain/encoded/EncodingModule.java
@@ -0,0 +1,46 @@
+package lsh.ext.gson.domain.encoded;
+
+import com.google.gson.TypeAdapterFactory;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import lsh.ext.gson.AbstractModule;
+import lsh.ext.gson.IBuilder0;
+import lsh.ext.gson.IModule;
+import lsh.ext.gson.ITypeAdapterFactory;
+
+public final class EncodingModule
+		extends AbstractModule {
+
+	@Getter
+	private static final IModule instance = Builder.create()
+			.build();
+
+	private EncodingModule(final TypeAdapterFactory... typeAdapterFactories) {
+		super(typeAdapterFactories);
+	}
+
+	@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+	@Accessors(fluent = true, chain = true)
+	public static final class Builder
+			implements IBuilder0<IModule> {
+
+		@Setter
+		private ITypeAdapterFactory<byte[]> primitiveByteArrayTypeAdapterFactory = EncodingTypeAdapterFactory.forPrimitiveByteArray(EncodingTypeAdapter.getDefaultForPrimitiveByteArrayAsBase64());
+
+		public static Builder create() {
+			return new Builder();
+		}
+
+		@Override
+		public IModule build() {
+			return new EncodingModule(
+					primitiveByteArrayTypeAdapterFactory
+			);
+		}
+
+	}
+
+}
