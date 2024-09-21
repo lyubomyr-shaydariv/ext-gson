5960981d7351087c0f1127e6bb3d7aa93d40fb49
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-09-21 13:38:32 +0300

    Added JSON double type adapter

---
+++ src/main/java/lsh/ext/gson/JsonDoubleTypeAdapter.java
@@ -0,0 +1,46 @@
+package lsh.ext.gson;
+
+import java.io.IOException;
+import java.util.function.Function;
+import javax.annotation.Nullable;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public final class JsonDoubleTypeAdapter<T>
+		extends TypeAdapter<T> {
+
+	private final Function<? super T, Double> toDouble;
+	private final Function<? super Double, ? extends T> fromDouble;
+
+	public static <T> TypeAdapter<T> getInstance(
+			final Function<? super T, Double> toDouble,
+			final Function<? super Double, ? extends T> fromDouble
+	) {
+		return new JsonDoubleTypeAdapter<T>(toDouble, fromDouble)
+				.nullSafe();
+	}
+
+	@Override
+	public void write(final JsonWriter out, final T value)
+			throws IOException {
+		@Nullable
+		final Double d = toDouble.apply(value);
+		if ( d == null ) {
+			out.nullValue();
+			return;
+		}
+		out.value(d);
+	}
+
+	@Override
+	public T read(final JsonReader in)
+			throws IOException {
+		return fromDouble.apply(in.nextDouble());
+	}
+
+}
---
+++ src/main/java/lsh/ext/gson/JsonPrimitiveDoubleTtypeAdapter.java
@@ -0,0 +1,40 @@
+package lsh.ext.gson;
+
+import java.io.IOException;
+import java.util.function.DoubleFunction;
+import java.util.function.ToDoubleFunction;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public final class JsonPrimitiveDoubleTtypeAdapter<T>
+		extends TypeAdapter<T> {
+
+	private final ToDoubleFunction<? super T> toDouble;
+	private final DoubleFunction<? extends T> fromDouble;
+
+	public static <T> TypeAdapter<T> getInstance(
+			final ToDoubleFunction<? super T> toDouble,
+			final DoubleFunction<? extends T> fromDouble
+	) {
+		return new JsonPrimitiveDoubleTtypeAdapter<T>(toDouble, fromDouble)
+				.nullSafe();
+	}
+
+	@Override
+	public void write(final JsonWriter out, final T value)
+			throws IOException {
+		out.value(toDouble.applyAsDouble(value));
+	}
+
+	@Override
+	public T read(final JsonReader in)
+			throws IOException {
+		return fromDouble.apply(in.nextDouble());
+	}
+
+}
