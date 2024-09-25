e070981bb5b149fd4f65f1ab1e4094c9a9c72d3b
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-09-25 19:36:15 +0300

    Google Guava cache builders now make use of the builder functional factories

--- src/main/java/lsh/ext/gson/ext/com/google/common/cache/GuavaCacheTypeBuilder.java
+++ src/main/java/lsh/ext/gson/ext/com/google/common/cache/GuavaCacheTypeBuilder.java
@@ -10,17 +10,7 @@ public final class GuavaCacheTypeBuilder {
 	public static <K, V, C extends Cache<K, V>> IBuilder2<K, V, C> forCache(
 			final C cache
 	) {
-		return new IBuilder2<>() {
-			@Override
-			public void accept(final K k, final V v) {
-				cache.put(k, v);
-			}
-
-			@Override
-			public C build() {
-				return cache;
-			}
-		};
+		return IBuilder2.of(cache::put, () -> cache);
 	}
 
 }
