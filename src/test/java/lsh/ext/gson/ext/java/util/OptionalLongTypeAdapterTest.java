932fa6919921cad76aa7ad14fa8805a50419551b
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2025-01-31 14:17:27 +0200

    WIP: Added Optional* tri-state tests

--- src/test/java/lsh/ext/gson/ext/java/util/OptionalLongTypeAdapterTest.java
+++ src/test/java/lsh/ext/gson/ext/java/util/OptionalLongTypeAdapterTest.java
@@ -4,8 +4,12 @@ import java.util.List;
 import java.util.OptionalLong;
 import javax.annotation.Nullable;
 
+import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
 import lsh.ext.gson.AbstractTypeAdapterTest;
+import lsh.ext.gson.Gsons;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.provider.Arguments;
 
 public final class OptionalLongTypeAdapterTest
@@ -33,6 +37,26 @@ public final class OptionalLongTypeAdapterTest
 						OptionalLong.empty()
 				)
 		);
+	}
+
+	@Test
+	public void testTristate() {
+		final TypeAdapter<OptionalLong> unit = OptionalLongTypeAdapter.getInstance();
+		final Gson gson = Gsons.Builders.createNormalized()
+				.registerTypeAdapter(OptionalLong.class, unit)
+				.create();
+		final Record record = gson.fromJson("{\"value\":-1,\"noValue\":null}", Record.class);
+		Assertions.assertEquals(OptionalLong.of(-1), record.value);
+		Assertions.assertEquals(OptionalLong.empty(), record.noValue);
+		Assertions.assertNull(record.undefined);
+	}
+
+	@SuppressWarnings({ "InstanceVariableMayNotBeInitialized", "OptionalUsedAsFieldOrParameterType" })
+	private static final class Record {
+
+		@SuppressWarnings("unused")
+		private OptionalLong value, noValue, undefined;
+
 	}
 
 }
